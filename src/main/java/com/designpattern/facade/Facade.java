dec5333a38c90c887c0ce4d92c9b204112a87a0a
Yen Hu <yen.hu@outlook.com>
2022-08-31 17:14:21 +0800

    Add facade pattern

---
+++ src/main/java/com/designpattern/facade/Facade.java
@@ -0,0 +1,5 @@
+package com.designpattern.facade;
+
+public class Facade {
+    
+}
---
+++ src/test/java/com/designpattern/FacadeTest.java
@@ -0,0 +1,19 @@
+package com.designpattern;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FacadeTest {
+    static final Logger LOGGER = LoggerFactory.getLogger(FacadeTest.class);
+    Random rand = new Random();
+
+    @Test
+    public void testFacade() {
+        LOGGER.info("Simple Pattern, Not Implemented");
+        
+    }
+
+}
