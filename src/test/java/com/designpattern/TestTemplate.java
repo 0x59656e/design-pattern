a66332fffcf9f3723f0870de54f40dd79b61c9f2
Yen Hu <yen.hu@outlook.com>
2022-09-02 10:02:10 +0800

    Add test template

---
+++ src/test/java/com/designpattern/TestTemplate.java
@@ -0,0 +1,14 @@
+package com.designpattern;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestTemplate {
+    final static Logger LOGGER = LoggerFactory.getLogger(TestTemplate.class);
+
+    @Test
+    public void name() {
+
+    }
+}
